e4cf2e88dbb5a1efa673581734f9d82accca7f6a
rongdo4897 <tunglam977@gmail.com>
2024-01-24 18:43:37 +0700

    writting TokenGenerator in tokengen

--- tokens/tokengen.go
+++ tokens/tokengen.go
@@ -1,1 +1,62 @@
 package tokens
+
+import (
+	"log"
+	"os"
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/rongdo4897/ecommerce-yt/database"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type SignedDetails struct {
+	Email      string
+	First_Name string
+	Last_name  string
+	Uid        string
+	jwt.StandardClaims
+}
+
+var UserData *mongo.Collection = database.UserData(database.Client, "Users")
+
+var SECRET_KEY = os.Getenv("SECRET_KEY")
+
+func TokenGenerator(email string, firstName string, lastName string, uid string) (signedtoken string, signedrefreshtoken string, err error) {
+	claims := &SignedDetails{
+		Email:      email,
+		First_Name: firstName,
+		Last_name:  lastName,
+		Uid:        uid,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(24)).Unix(),
+		},
+	}
+
+	refreshclaims := &SignedDetails{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(168)).Unix(),
+		},
+	}
+
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		return "", "", err
+	}
+
+	refreshtoken, err := jwt.NewWithClaims(jwt.SigningMethodHS384, refreshclaims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+
+	return token, refreshtoken, err
+}
+
+func ValidateToken() {
+
+}
+
+func UpdateAllTokens() {
+
+}
