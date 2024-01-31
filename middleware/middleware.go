328eda921101350e8edefbfeae8b35f8c95fc035
rongdo4897 <tunglam977@gmail.com>
2024-01-31 18:19:23 +0700

    writting Authentication in middleware

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,1 +1,30 @@
 package middleware
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	token "github.com/rongdo4897/ecommerce-yt/tokens"
+)
+
+func Authentication() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		clientToken := c.Request.Header.Get("token")
+		if clientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "No authorization header token"})
+			c.Abort()
+			return
+		}
+
+		claims, err := token.ValidateToken(clientToken)
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
+			c.Abort()
+			return
+		}
+
+		c.Set("email", claims.Email)
+		c.Set("uid", claims.Uid)
+		c.Next()
+	}
+}
